6b4f80b9789d04452a9adfa1e54413a99b3b7839
Mateusz Kłosiński <mkklosin@gmail.com>
2017-11-16 22:39:19 +0100

    Bubbling

---
+++ Day 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,12 @@
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+    console.log(this.classList.value);
+    e.stopPropagation();
+}
+
+document.body.addEventListener
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: true
+}));
